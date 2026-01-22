9cbe973a3ddaaa7d8e2993d29f5c57e2ad778e18
Marta Llabrés <marta.llab.daw@gmail.com>
2026-01-22 20:55:49 +0100

    Create entity

---
+++ CourselyLabs-back/src/main/java/com/courselylabs/CourselyLabs/entity/CategoriaEntity.java
@@ -0,0 +1,37 @@
+package com.courselylabs.CourselyLabs.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="categories")
+public class CategoriaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name="slug", nullable = false, unique = true)
+    private String slug;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="created_at", nullable = false)
+    private Timestamp createdAt;
+}
