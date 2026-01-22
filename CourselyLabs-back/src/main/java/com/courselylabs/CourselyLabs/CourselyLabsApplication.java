74be68fe49866264df5c547f15112ae30d5fdb33
NicoMisz <nicommisz@gmail.com>
2026-01-22 19:57:40 +0100

    init backend spring boot

---
+++ CourselyLabs-back/src/main/java/com/courselylabs/CourselyLabs/CourselyLabsApplication.java
@@ -0,0 +1,13 @@
+package com.courselylabs.CourselyLabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourselyLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourselyLabsApplication.class, args);
+	}
+
+}
---
+++ CourselyLabs-back/src/test/java/com/courselylabs/CourselyLabs/CourselyLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courselylabs.CourselyLabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourselyLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
