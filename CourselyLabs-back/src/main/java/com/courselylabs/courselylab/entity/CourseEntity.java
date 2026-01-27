0e010524b84eb743ca2f8712a36e1763a590031d
NicoMisz <nicommisz@gmail.com>
2026-01-27 20:25:05 +0100

    revision

--- CourselyLabs-back/src/main/java/com/courselylabs/courselylab/entity/CourseEntity.java
+++ CourselyLabs-back/src/main/java/com/courselylabs/courselylab/entity/CourseEntity.java
@@ -1,15 +1,24 @@
 package com.courselylabs.courselylab.entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
@@ -38,7 +47,7 @@ public class CourseEntity {
     @Column(name = "thumbnail_url", length = 500)
     private String thumbnailUrl;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)//TODO quiero entender
     @JoinColumn(name = "category_id")
     private CategoriaEntity category;
 
