9ab1b7a73286236d692984e90a5bc5578535c31c
Marta Llabrés <marta.llab.daw@gmail.com>
2026-02-19 19:57:09 +0100

    Funciona!

--- CourselyLabs-front/src/views/HomeView.vue
+++ CourselyLabs-front/src/views/HomeView.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <q-page class="flex bg-white text-black">
+  <q-page class="flex bg-white text-black justify-center items-center">
     <img
       alt="Quasar logo"
-      src=""
+      src="https://pbs.twimg.com/media/C47_OEEWIAAYanY.jpg"
       style="width: 200px; height: 200px"
     />
     
