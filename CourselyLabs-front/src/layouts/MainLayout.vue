74b1dae4fa7d670e764b60fa30d591f18934aee3
NicoMisz <nicommisz@gmail.com>
2026-02-26 20:54:13 +0100

    Update main layout

--- CourselyLabs-front/src/layouts/MainLayout.vue
+++ CourselyLabs-front/src/layouts/MainLayout.vue
@@ -1,43 +1,118 @@
 <template>
-    <q-layout
-        view="hHr LpR lff"
-    >
-        <q-header elevated>
-            <q-toolbar>
-                <q-btn flat @click="drawerLeft = !drawerLeft" round dense icon="menu" />
+  <q-layout view="hHh Lpr fFf" class="bg-white">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="toggleLeftDrawer"
+          aria-label="Menu"
+          icon="menu"
+        />
 
-                <q-toolbar-title> CourselyLab - Header</q-toolbar-title>
+                <q-toolbar-title> CourselyLabs - Header</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
 
-                <div>Quasar v{{ $q.version }}</div>
-            </q-toolbar>
-        </q-header>
-
-        <q-drawer v-model="drawerLeft" show-if-above bordered 
-            :mini="miniState"
-            @mouseenter="miniState = false"
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-grey-2"
+      :mini="miniState"
+      @mouseenter="miniState = false"
             @mouseleave="miniState = true"
             :width="250"
             :mini-width="60"
-            class="bg-white"
-            overlay
-            :breakpoint="1024"
-        >
+    >
 
-            <q-list :class="$q.dark.isActive ? 'text-white' : 'text-black'">
-                <q-item-label header class="text-weight-bold"> Essential Links </q-item-label>
 
-                <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-            </q-list>
-        </q-drawer>
 
-        <q-page-container class="container">
-            <router-view />
-        </q-page-container>
-    </q-layout>
 
+
+
+    
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable target="_blank" rel="noopener" href="https://quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Docs</q-item-label>
+            <q-item-label caption>https://quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable target="_blank" rel="noopener" href="https://github.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>GitHub</q-item-label>
+            <q-item-label caption>github.com/quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable target="_blank" rel="noopener" href="http://chat.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="chat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Discord Chat Channel</q-item-label>
+            <q-item-label caption>https://chat.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable target="_blank" rel="noopener" href="https://forum.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="record_voice_over" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Forum</q-item-label>
+            <q-item-label caption>https://forum.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable target="_blank" rel="noopener" href="https://twitter.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="rss_feed" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Twitter</q-item-label>
+            <q-item-label caption>@quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable target="_blank" rel="noopener" href="https://facebook.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="public" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Facebook</q-item-label>
+            <q-item-label caption>@QuasarFramework</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          CourselyLabs - Todos los derechos reservados
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
 </template>
 
+
+
+
 <script setup lang="ts">
+
+
+
+
     import { ref } from 'vue';
     import { useQuasar } from 'quasar';
     import EssentialLink from '../components/EssentialLinks.vue';
@@ -84,7 +159,13 @@
             icon: 'las la-external-link-alt',
             link: 'https://www.google.com/',
         },
+        
     ]);
+    const leftDrawerOpen = ref(false)
+    function toggleLeftDrawer () {
+      leftDrawerOpen.value = !leftDrawerOpen.value
+    }
+    
 </script>
 
 <style scoped>
