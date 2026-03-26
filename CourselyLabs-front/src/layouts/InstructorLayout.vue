c3a0b78a287134320b3413c7092b5adf6211ff2e
NicoMisz <nicommisz@gmail.com>
2026-03-26 20:09:20 +0100

    feat: InstructorLayout deja volver a inicio

--- CourselyLabs-front/src/layouts/InstructorLayout.vue
+++ CourselyLabs-front/src/layouts/InstructorLayout.vue
@@ -12,6 +12,13 @@
 
     <q-drawer v-model="drawerOpen" bordered show-if-above :width="240" class="bg-grey-1">
       <q-list class="q-pt-md">
+        <q-item clickable to="/">
+          <q-item-section avatar><q-icon name="arrow_back" /></q-item-section>
+          <q-item-section>Volver al sitio</q-item-section>
+        </q-item>
+
+        <q-separator class="q-my-sm" />
+
         <q-item clickable to="/instructor/cursos" active-class="text-primary" exact>
           <q-item-section avatar><q-icon name="dashboard" /></q-item-section>
           <q-item-section>Mis cursos</q-item-section>
